59bcf5bc67628f8a415fda5ee9364d53974fffac
ossamakada75 <ossamakada75@gmail.com>
2024-06-20 15:23:56 +0100

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-""" returns the top ten posts """
-
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """ function that queries the Reddit API and
-    prints the titles of the first 10
-    hot posts listed for a given subreddit """
-    if type(subreddit) is not str or subreddit is None:
-        print("None")
-        return None
-    apiurl = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    header = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/aboubakrtaibi)"
-        }
-    res = requests.get(apiurl, headers=header, allow_redirects=False)
-    if res.status_code == 200:
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-        print(res)
-        data = res.json()
-        if 'data' in data and 'children' in data['data']:
-            posts = data['data']['children']
-            for _, post in enumerate(posts[:10], start=1):
-                title = post['data']['title']
-                print(title)
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        print("None")
-        return 1
+        print(None)
